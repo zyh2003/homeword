5ff48523610a5ce73be6a0042e4e137a71fe267e
小怡白耶 <3530953858@qq.com>
2022-07-18 21:30:22 +0800

    作业

---
+++ src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <yangButton>默认按钮</yangButton>
+    <yangButton type="primary"  >成功按钮</yangButton>
+    <yangButton type="danger" >危险按钮</yangButton>
+    <yangButton type="warning" >警告按钮</yangButton>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+  data () {
+    return {
+      flag: false
+    }
+  },
+  components: {
+    yangButton: () => import('../components/button/index.vue')
+  },
+  methods: {
+    handleSubmit () {
+      this.flag = true
+      setTimeout(() => {
+        console.log('123')
+        this.flag = false
+      }, 3000)
+    }
+  }
+}
+</script>
